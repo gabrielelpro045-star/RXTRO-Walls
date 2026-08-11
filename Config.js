6244de2eac18d8c8b73bec8ea00839717f15a040
gabrielelpro045-star <gabrielelpro045@gmail.com>
2026-08-11 15:51:46 -0400

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,2 +1,4 @@
-const SUPABASE_URL = "https://etpofwsehrluwrfvdrxw.supabase.co";
-const SUPABASE_KEY = "sb_publishable_nLb_5_amJGlSPcAt5z89Lw_MeNzlVVj";
+window.RXTRO_CONFIG = {
+  SUPABASE_URL: "https://etpofwsehrluwrfvdrxw.supabase.co",
+  SUPABASE_KEY: "sb_publishable_nLb_5_amJGlSPcAt5z89Lw_MeNzlVVj"
+};
